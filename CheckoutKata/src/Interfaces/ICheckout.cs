c90ddaae601959fa035a777d8f5b925dcb91fe6a
Alexandru-Matei Coman <Alexandru-Matei.Coman@verastar.co.uk>
2021-04-30 14:30:38 +0100

    added Checkout class and its interface. Added interface folder

---
+++ CheckoutKata/src/Interfaces/ICheckout.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutKata.src.Interfaces
+{
+    interface ICheckout
+    {
+        void Scan(string item);
+        int GetTotalPrice();
+    }
+}
